5d2f402f32e8af4db21c5d3bf6a10e38b2787b2d
lzm <lzm6@fox.mail>
2021-09-06 13:32:06 +0800

    修复了bug

--- flink-app/src/main/java/com/nju/ics/Models/TrafficRecord.java
+++ flink-app/src/main/java/com/nju/ics/Models/TrafficRecord.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class TrafficRecord extends AbstractModel {
     /**入口、出口:通行标识ID 门架：通行标识 ID */
-    private String id;
+    public String id;
     /** class Media */
     public String mediaId;
     /** class GantryRecord */
